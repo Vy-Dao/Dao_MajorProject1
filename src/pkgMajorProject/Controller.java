5cca60135652d0cb8aac1812da9f144e2c7381a2
Vy <o0oalexo0o321@gmail.com>
2019-10-10 22:07:41 -0500

    Fixed some errors

--- src/pkgMajorProject/Controller.java
+++ src/pkgMajorProject/Controller.java
@@ -1,15 +1,26 @@
 package pkgMajorProject;
 
+import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.ImageView;
 
 public class Controller {
+
+    //Creating Buttons, radio, checkbox, all of the control action
+    @FXML Button btnSubmit; @FXML Button btnPrintInv; @FXML Button btnPrintInvTable; @FXML Button btnClear;
+    @FXML ImageView imgItem;
+    @FXML TextField txfItemName; @FXML TextField txfItemCost; @FXML TextField txf;
     //Creating products
     Product p1 = new Product("Potato",10,5.00f);
     //
+    public void rdoAnhChkHandle(Event e){
 
-    public void btnAcctionHandel(Event e){
+    }
+    public void btnActionHandel(ActionEvent e){
     }
 
 }
